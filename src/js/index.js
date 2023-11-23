c96119d0b2a539ed9bca91dedfc43bbf660c7532
Erick Rodrigues Pacheco <44420998+Erick-RPacheco@users.noreply.github.com>
2023-11-22 23:26:30 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,19 +4,16 @@ const botaoDeBusca = document.querySelector(".btn-busca");
 botaoDeBusca.addEventListener("click", async () => {
     const cidade = document.getElementById("input-busca").value
 
-    if(!cidade) return
-
     const dados = await buscarDadosDaCidade(cidade);
 
-    if(dados) preencherDadosNaTela(dados, cidade)
+    preencherDadosNaTela(dados, cidade)
 });
 
 async function buscarDadosDaCidade(cidade) {
-    const apiUrl = `https://api.weatherapi.com/v1/cuFrrent.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
 
     const resposta = await fetch(apiUrl)
-
-    if(resposta.status !== 200) return;
+    
 
     const dados = resposta.json();
 
